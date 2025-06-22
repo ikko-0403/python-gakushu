934d2166fa1e2c6359255a7ee285d86acf6311b1
ikkow <doikazumitsu@ikkownoMacBook-Pro.local>
2025-06-22 13:40:54 +0900

    今日の成果

--- ダックタイピング.py
+++ ダックタイピング.py
@@ -1,8 +1,43 @@
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+
+    def drive(self):
+        if self.age >= 18:
+            print('OK')
+        else:
+            raise Exception('No drive')
+
+class Baby(Person):
+    def __init__(self, age=1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self, age=18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+baby = Baby()
+adult = Adult()    
+
+
+
 class Car(object):
     def __init__(self, model=None):
         self.model = model 
     def run(self):# これで model をインスタンス変数として保持
         print('run')
+    def ride(self, person):
+        person.drive()
+
+car = Car()
+car.ride(adult)
+
+
 
 class ToyotaCar(Car):
     def run(self):
