bc8030ab880497c06fac1f11368f5e7fbe9069b6
ikko <ikkoikko@ikkonoMacBook-Pro.local>
2025-08-03 16:32:05 +0900

    feat: Add today's Flask and SQLite practice

---
+++ WEBとネットワーク/templates/XMLRPC.py
@@ -0,0 +1,9 @@
+from xmlrpc.server import SimpleXMLRPCServer#PythonでRPC（Remote Procedure Call）サーバーを作る
+
+with SimpleXMLRPCServer(('127.0.0.1', 8000)) as server:
+
+    def add_num(x, y):
+        return(x + y)
+    
+    server.register_function(add_num, 'add_num')
+    server.serve_forever()
